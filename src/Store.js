de191557d976ea493d9dc6e78b85c8fea2936748
Ben Wharnsby <ben.wharnsby@imaginativesoftware.co.uk>
2020-01-23 09:38:54 +0000

    first commit

---
+++ src/Store.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+export const CTX = React.createContext();
+
+const initState = {
+  general: [
+    { from: "ben", msg: "hello" },
+    { from: "ben", msg: "hello" },
+    { from: "ben", msg: "hello" }
+  ],
+  soccer: [
+    { from: "ben", msg: "hello" },
+    { from: "ben", msg: "hello" },
+    { from: "ben", msg: "hello" }
+  ]
+};
+
+function reducer(state, action) {
+  const { from, msg, topic } = action.payload;
+  switch (action.type) {
+    case "RECEIVE_MESSAGE":
+      return {
+        ...state,
+        [topic]: [
+          ...state[topic],
+          {
+            from,
+            msg
+          }
+        ]
+      };
+    default:
+      return state;
+  }
+}
+
+export default function Store(props) {
+  const reducerHook = React.useReducer(reducer, initState);
+  return <CTX.Provider value={reducerHook}>{props.children}</CTX.Provider>;
+}
